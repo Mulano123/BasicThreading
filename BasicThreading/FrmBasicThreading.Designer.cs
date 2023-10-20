aab9fdfa92da8dc0924633901a92f20f42f1c869
mulano123 <joshuamulano26@gmail.com>
2023-10-20 18:53:05 +0800

    Add project files.

---
+++ BasicThreading/FrmBasicThreading.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace BasicThreading
+{
+    partial class FrmBasicThreading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblThreadStart = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(189, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblThreadStart
+            // 
+            this.lblThreadStart.AutoSize = true;
+            this.lblThreadStart.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThreadStart.Location = new System.Drawing.Point(99, 108);
+            this.lblThreadStart.Name = "lblThreadStart";
+            this.lblThreadStart.Size = new System.Drawing.Size(309, 36);
+            this.lblThreadStart.TabIndex = 1;
+            this.lblThreadStart.Text = "-Before starting thread-";
+            // 
+            // FrmBasicThreading
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(524, 450);
+            this.Controls.Add(this.lblThreadStart);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmBasicThreading";
+            this.Text = "BasicThreading";
+            this.Load += new System.EventHandler(this.FrmBasicThreading_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblThreadStart;
+    }
+}
---
+++ BasicThreading/FrmBasicThreading.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicThreading
+{
+    public partial class FrmBasicThreading : Form
+    {
+
+        public FrmBasicThreading()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmBasicThreading_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(lblThreadStart.Text);
+
+            ThreadStart delThread= new ThreadStart(MyThreadClass.Thread1);
+            Thread ThreadA = new Thread(delThread);
+            Thread ThreadB = new Thread(delThread);
+
+            ThreadA.Name = "Thread A Process";
+            ThreadB.Name = "Thread B Process";
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+
+            lblThreadStart.Text = "-End of thread-";
+            Console.WriteLine(lblThreadStart.Text);
+        }        
+    }
+}
